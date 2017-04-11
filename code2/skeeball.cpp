e3663444e960b3a0df0cfe9f5fc392bfc50b2dfa
Natalie Rakoski <nrakoski3@gatech.edu>
2017-04-11 00:28:19 -0400

    started code 2 assignment

---
+++ code2/skeeball.cpp
@@ -0,0 +1,15 @@
+//
+//  3020_code1.cpp
+//
+//
+//  Created by Natalie Rakoski on 3/13/17.
+//
+//
+
+#include "skeeball.hpp"
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <bitset>
+using namespace std;
